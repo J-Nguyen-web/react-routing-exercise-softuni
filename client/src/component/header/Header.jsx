b12a3e035e860aba817594d96902c783ded87cf2
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-11-29 00:39:31 +0200

    footer header added

---
+++ client/src/component/header/Header.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+
+
+export default function Header() {
+    return (
+        <header>
+        {/* <!-- Navigation --> */}
+            <nav>
+                <Link className="home" to="/"> <img src="./images/logo.png" alt="logo" /> </Link>
+                <Link to="/games">Catalog</Link>
+                {/* <!-- Logged-in users --> */}
+                <div id="user">
+                    <Link to="/create">Add Game</Link>
+                    <Link to="/logout">Logout</Link>
+                </div>
+                {/* <!-- Guest users --> */}
+                <div id="guest">
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </nav>
+        </header>
+    );
+}
