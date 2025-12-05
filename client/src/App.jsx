00b02fb195eedfc3a446595d4bcfff1bc5413240
J-Nguyen-web <map.genshin.impacts@gmail.com>
2025-12-05 13:30:44 +0200

    add routes and new persistant entries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Footer from './component/footer/Footer.jsx'
 import Header from './component/header/Header.jsx'
 import Home from './component/home/Home.jsx'
 import Catalog from './component/games/Catalog.jsx'
+import Details from './component/details/Details.jsx'
+import Create from './component/create-game/Create.jsx'
 
 function App() {
 
@@ -15,6 +17,9 @@ function App() {
 
         {/* тъй като в Home.jsx има Link към /games, се описва тук в Routes */}
         <Route path='/games' element={<Catalog />} />
+        
+        <Route path='/games/:gameId/details' element={<Details />} />
+        <Route path='/create' element={<Create />} />
       </Routes>
       
       <Footer />
